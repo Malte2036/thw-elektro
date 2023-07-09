b9eff0dfe7bdc9c0d7c6fc701183e20c08cd0c1a
Malte2036 <malte2036@gmail.com>
2023-07-09 14:07:21 +0200

    :lipstick: Add tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        thw: {
+          DEFAULT: "#120A8F",
+          50: "#f8f7fe",
+          100: "#eeedfe",
+          200: "#dedcfd",
+          300: "#c8c4fb",
+          400: "#a49ef8",
+          500: "#7f77f5",
+          600: "#5e54f3",
+          700: "#4c40f2",
+          800: "#1f11ee",
+          900: "#1a0ec7",
+        },
       },
     },
   },
   plugins: [],
-}
+};
