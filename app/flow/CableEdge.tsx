5827ed650e6d43490c09986c2f2531efc99a17ff
Malte2036 <malte2036@gmail.com>
2023-07-15 01:50:01 +0200

    :lipstick: Add position absolute to Edge Buttons

--- app/flow/CableEdge.tsx
+++ app/flow/CableEdge.tsx
@@ -45,7 +45,7 @@ export default function CableEdge(edgeProps: EdgeProps<CableEdgeData>) {
           className="nodrag nopan flex flex-col gap-1 items-center"
         >
           {edgeProps.selected && (
-            <div className="flex flex-row gap-1">
+            <div className="flex flex-row gap-1 absolute -top-8">
               <Button
                 type="secondary"
                 onClick={() => edgeProps.data?.nextLength(edgeProps.data.cable)}
@@ -67,10 +67,10 @@ export default function CableEdge(edgeProps: EdgeProps<CableEdgeData>) {
             </div>
           )}
           <div
-            className={`edgebutton bg-white ${
+            className={`bg-white ${
               isVoltageDropTooHigh(edgeProps.data?.cable.voltageDrop ?? 0)
                 ? "border-red-600 text-red-600 font-bold border-4"
-                : "border-black  border-2"
+                : "border-black border-2"
             } rounded-md px-1 py-0.5 flex flex-col items-center justify-center`}
           >
             <div>{edgeProps.data?.cable.length}m</div>
