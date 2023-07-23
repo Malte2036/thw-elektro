edcacffe97116e87efde03d2457e01dba9de3540
Malte2036 <malte2036@gmail.com>
2023-07-23 15:13:06 +0200

    Revert ":truck: Add redirect button to /flow page"
    This reverts commit 2ff8ea39161d700dbb9bd21d05b2f2e1e0fc3df9.

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import { Button } from "./components/Button.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/flow"
-          element={
-            <div className="h-screen w-screen flex flex-col items-center justify-center gap-2">
-              <div className="text-xl">
-                Die "Elektro Flow" Seite wurde verschoben.
-              </div>
-              <Button
-                type="primary"
-                onClick={() => {
-                  window.location.href = "/";
-                }}
-              >
-                Zur neuen Seite
-              </Button>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
