8f4907dc5e926a1f4d98a987524c24e44923c77b
Malte2036 <malte2036@gmail.com>
2023-08-01 20:30:42 +0200

    :lipstick: Fix Footer for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-    return <div className="flex flex-row justify-center gap-2">
+    return <div className="flex flex-row justify-center gap-2 w-max">
         <div>©2023 Malte Sehmer</div>
         <div className="text-gray-400">|</div>
         <a href='https://thw-tools.de/impressum' target="_blank" className="underline">Impressum</a>
