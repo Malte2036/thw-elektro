6d757af297435df32b2d25a2e16b5a9c83ccdaf7
Malte2036 <malte2036@gmail.com>
2023-10-24 14:25:18 +0200

    :sparkles: Implement control fitView

--- src/FlowPage.tsx
+++ src/FlowPage.tsx
@@ -383,7 +383,7 @@ export default function FlowPage() {
           fitView
         >
           <ReactFlow.Background />
-          <ReactFlow.Controls />
+          <ReactFlow.Controls onFitView={rfInstance?.fitView} />
           <ReactFlow.Panel position="top-right">
             <div className="flex flex-col gap-2">
               <Button
